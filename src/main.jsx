48d6e12c0729d8f539ce9c6ac39797fcb51a62db
iGeek <jesus.cardenas.d@gmail.com>
2024-01-20 02:12:30 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { TemaProvider } from './components/context/TemaContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TemaProvider>
     <App />
-    </TemaProvider>
   </React.StrictMode>,
 )
